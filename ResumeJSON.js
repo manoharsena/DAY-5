f0b699b4041bd5ae58be133a5f2d604a85e1925b
manoharsena <manoharsena@gmail.com>
2023-12-30 10:46:22 +0530

    Formatted sctipt file

--- ResumeJSON.js
+++ ResumeJSON.js
@@ -1,29 +1,37 @@
 const resume = {
-    "NAME" : "Senapathi Manoharan",
-    "EMAIL" : "manoharsena@gmail.com",
-    "MOBILE" : "+91 99526 70947",
+  NAME: "Senapathi Manoharan",
+  EMAIL: "manoharsena@gmail.com",
+  MOBILE: "+91 99526 70947",
 
-    "LOCATION" : [{"City":"Erode",
-                   "State":"Tamil Nadu",
-                   "Pincode":"638151"}],
-    
-    "EDUCATION" : [{"University":"Anna University, Chennai",
-                   "College":"Government College of Technolody, Coimbatore.",
-                   "Course":"M.E., (CSE)",
-                   "Duration":"2017-2019",
-                   "CGPA":"7.36"},
+  LOCATION: [{ City: "Erode", State: "Tamil Nadu", Pincode: "638151" }],
 
-                   {"University":"Anna University, Chennai",
-                   "College":"Karur College of Engineering, Karur.",
-                   "Course":"B.E., (CSE)",
-                   "Duration":"2012-2015",
-                   "CGPA":"7.41"}],
+  EDUCATION: [
+    {
+      University: "Anna University, Chennai",
+      College: "Government College of Technolody, Coimbatore.",
+      Course: "M.E., (CSE)",
+      Duration: "2017-2019",
+      CGPA: "7.36",
+    },
 
-    "SKILLS" : [{"Programming":"C, C++, Java",
-                 "Softskill":"Problem solving, Logical Thinking",
-                 "Tools":"MS-office, VS Code"}],
+    {
+      University: "Anna University, Chennai",
+      College: "Karur College of Engineering, Karur.",
+      Course: "B.E., (CSE)",
+      Duration: "2012-2015",
+      CGPA: "7.41",
+    },
+  ],
 
-    "LANGUAGE" : [{"language":"Tamil, English, Hindi"}],
-}
+  SKILLS: [
+    {
+      Programming: "C, C++, Java",
+      Softskill: "Problem solving, Logical Thinking",
+      Tools: "MS-office, VS Code",
+    },
+  ],
+
+  LANGUAGE: [{ language: "Tamil, English, Hindi" }],
+};
 
 console.log(resume);
